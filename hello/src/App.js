f62be06988131034b4ea55e9aec1f7c01ae57b94
this-is-yello <yello_3617@naver.com>
2022-04-06 12:53:17 +0900

    react

---
+++ hello/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+let 인사말 = "안녕하세요";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="hi">
+        <p> {인사말} </p>
+        <p>뇽</p>
+        <p>안</p>
+        <p>뇽</p>
+        <p>안</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
