d194f65ab9b444be6cdc8edb65ddf4b8803b6f74
this-is-yello <yello_3617@naver.com>
2022-04-07 12:53:48 +0900

    Update App.js

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,19 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-
-let 인사말 = "안녕하세요";
+import { useState } from 'react';
 
 function App() {
+
+  let [변수명,변수수정도구] = useState("고기 잘 굽는 법");
+  let [count,setCount] = useState(0);
+
   return (
     <div className="App">
-      <div className="hi">
-        <p> {인사말} </p>
-        <p>뇽</p>
-        <p>안</p>
-        <p>뇽</p>
-        <p>안</p>
-      </div>
+      <Post title={변수명} count={count} setCount={setCount}></Post>
+      <Post title={변수명} count={count} setCount={setCount}></Post>
+      <Post title={변수명} count={count} setCount={setCount}></Post>
+      <Post title={변수명} count={count} setCount={setCount}></Post>
     </div>
+  );
+}
+
+function Post(props) {
+  // let [변수명,변수수정도구] = useState("고기 잘 굽는 법");
+  // let [count,setCount] = useState(0);
+
+  return(
+    <div className="Post">
+      <h2>{props.title}</h2>
+      <p>잘 굽는 사람환테 구워달라고 합니다.
+        <Like count={props.count} setCount={props.setCount}></Like>
+      </p>
+      <span> {props.count} </span>
+    </div>
+  );
+}
+
+function Like(props) {
+  return(
+    <>
+      <span onClick={() => {props.setCount (props.count+1)}}>ㅇㅇ</span>
+      <span onClick={() => {props.setCount (props.count-1)}}>ㄴㄴ</span>
+    </>
   );
 }
 
