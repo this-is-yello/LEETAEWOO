65c1a51a016264da9d502e9b8a8acf7b5ffff2ae
this-is-yello <yello_3617@naver.com>
2022-04-08 12:52:17 +0900

    Update App.js

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,44 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import Post from './Post';
+import Calculator from './js/Calculator';
+
 
 function App() {
-
-  let [변수명,변수수정도구] = useState("고기 잘 굽는 법");
-  let [count,setCount] = useState(0);
-
+  // let [포스트제목, 포스트제목수정도구] = useState(["고기 잘 굽는 법", "고기 맛없게 굽지마"]);
+  // let [포스트내용,포스트내용수정도구] = useState(["잘 굽는 사람에게 구워달라고 한다.", "참지못해"]);
+  
   return (
-    <div className="App">
-      <Post title={변수명} count={count} setCount={setCount}></Post>
-      <Post title={변수명} count={count} setCount={setCount}></Post>
-      <Post title={변수명} count={count} setCount={setCount}></Post>
-      <Post title={변수명} count={count} setCount={setCount}></Post>
-    </div>
+    // <div className="App">
+    //   <Post title={포스트제목[0]} content={포스트내용[0]}></Post>
+    //   <Post title={포스트제목[1]} content={포스트내용[1]}></Post>
+    //   <Post title={포스트제목[0]} content={포스트내용[1]}></Post>
+    //   <Post title={포스트제목[1]} content={포스트내용[0]}></Post>
+    // </div>
+    <Calculator/>
   );
 }
 
-function Post(props) {
-  // let [변수명,변수수정도구] = useState("고기 잘 굽는 법");
-  // let [count,setCount] = useState(0);
+// function Post(props) {
+//   // let [변수명,변수수정도구] = useState("고기 잘 굽는 법");
+//   // let [count,setCount] = useState(0);
+  
+//   let [count,setCount] = useState(0);
+//   return(
+//     <div className="Post">
+//       <h2>{props.title}</h2>
+//       <p>{props.content}
+//         <Like count={count} setCount={setCount}></Like>
+//       </p>
+//       <span> {count} </span>
+//     </div>
+//   );
+// }
 
-  return(
-    <div className="Post">
-      <h2>{props.title}</h2>
-      <p>잘 굽는 사람환테 구워달라고 합니다.
-        <Like count={props.count} setCount={props.setCount}></Like>
-      </p>
-      <span> {props.count} </span>
-    </div>
-  );
-}
+// function Like(props) {
+//   return(
+//     <>
+//       <span onClick={() => {props.setCount (props.count+1)}}>ㅇㅇ</span>
+//       <span onClick={() => {props.setCount (props.count-1)}}>ㄴㄴ</span>
+//     </>
+//   );
+// }
 
-function Like(props) {
-  return(
-    <>
-      <span onClick={() => {props.setCount (props.count+1)}}>ㅇㅇ</span>
-      <span onClick={() => {props.setCount (props.count-1)}}>ㄴㄴ</span>
-    </>
-  );
-}
+
 
 export default App;
